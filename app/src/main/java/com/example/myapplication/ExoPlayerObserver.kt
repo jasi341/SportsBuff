39b450afefb08573dac1e596d3cb81083e360d0d
Jasmeet Singh <sjasmeet438@gmail.com>
2023-04-20 15:24:00 +0530

    branch2

--- app/src/main/java/com/example/myapplication/ExoPlayerObserver.kt
+++ app/src/main/java/com/example/myapplication/ExoPlayerObserver.kt
@@ -9,21 +9,16 @@ import com.google.android.exoplayer2.DefaultLoadControl
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 
-class ExoPlayerObserver(
-    lifecycleOwner: LifecycleOwner,
-    private val context: Context
-) :LifecycleObserver{
+class ExoPlayerObserver(lifecycleOwner: LifecycleOwner, private val context: Context) :
+    LifecycleObserver {
 
-    var player :SimpleExoPlayer
+    var player: SimpleExoPlayer
     lateinit var videoProgressTracker: ProgressTracker
-    private val loadControl by lazy {DefaultLoadControl()}
+
+    private val loadControl by lazy { DefaultLoadControl() }
 
     init {
-        player = SimpleExoPlayer
-            .Builder(context)
-            .setLoadControl(loadControl)
-            .build()
-
+        player = SimpleExoPlayer.Builder(context).setLoadControl(loadControl).build()
         player.repeatMode = Player.REPEAT_MODE_ALL
         lifecycleOwner.lifecycle.addObserver(this)
     }
@@ -34,18 +29,13 @@ class ExoPlayerObserver(
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    fun resumeVideo(){
+    fun resumeVideo() {
         player.playWhenReady = true
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    fun stop(){
+    fun stop() {
         videoProgressTracker.unsubscribe()
         player.release()
     }
-
-
-
-
 }
-
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,93 +2,92 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.buffup.sdk.models.stream.StreamResultListener
 import com.buffup.sdk.models.stream.StreamSummary
-import com.example.myapplication.databinding.ActivityMainBinding
+import com.buffup.sdk.ui.BuffView
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
-import org.koin.core.component.KoinComponent
+import com.google.android.exoplayer2.ui.PlayerView
 import java.util.*
 
-class MainActivity : AppCompatActivity(),KoinComponent,PositionListener, Player.Listener {
+class MainActivity : AppCompatActivity(), PositionListener, Player.Listener {
 
-    private lateinit var binding: ActivityMainBinding
     private lateinit var expPlayerObserver: ExoPlayerObserver
-
+    private lateinit var buffView: BuffView
+    private lateinit var playerView: PlayerView
 
     private var startDateTimestamp: Long? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
-        expPlayerObserver = ExoPlayerObserver(this,this)
-
-        binding.buffView.startStream(
-            providerId = streamID,
-            listener = object:StreamResultListener{
-                override fun onError(t: Throwable?) {
-                    Toast.makeText(this@MainActivity, "Something Went Wrong", Toast.LENGTH_SHORT).show()
-                }
-
+//        setTheme(R.style.FullscreenTheme)
+        setContentView(R.layout.activity_main)
+        initViews()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        expPlayerObserver = ExoPlayerObserver(this, this)
+        buffView.startStream(
+            providerId = PROVIDER_ID,
+            listener = object : StreamResultListener {
                 override fun onSuccess(data: StreamSummary) {
                     startVideo()
+                }
+
+                override fun onError(t: Throwable?) {
+                    Toast.makeText(this@MainActivity, "Stream doesn't exist", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
             }
         )
     }
 
-    private fun startVideo(){
-        val demoVideo = url
-
-        val mediaItem:MediaItem = MediaItem.fromUri(Uri.parse(demoVideo))
-
-        expPlayerObserver.player.setMediaItem(mediaItem)
-        expPlayerObserver.player.prepare()
-
-        expPlayerObserver.videoProgressTracker = ProgressTracker(
-            expPlayerObserver.player,this
-        )
-
+    private fun initViews() {
+        buffView = findViewById(R.id.buffView)
+        playerView = findViewById(R.id.playerView)
     }
 
-    override fun progress(position: Long) {
-        binding.buffView.setVideoProgress(position)
-        startDateTimestamp?.let { startDateTime ->
-            val currentPlayBackTime =
-                startDateTime +position
-            binding.buffView.provideSync(currentPlayBackTime)
-        }
+    private fun startVideo() {
+        val demoVideo =
+            "https://buffup-public.s3.eu-west-2.amazonaws.com/video/fwc18_final_footage.mp4"
+        val mediaItem: MediaItem = MediaItem.fromUri(Uri.parse(demoVideo))
+        expPlayerObserver.player.setMediaItem(mediaItem)
+        expPlayerObserver.player.prepare()
+        expPlayerObserver.player.addListener(this)
+
+        playerView.keepScreenOn = true
+        playerView.player = expPlayerObserver.player
+        expPlayerObserver.videoProgressTracker = ProgressTracker(expPlayerObserver.player, this)
     }
 
     override fun onPlaybackStateChanged(state: Int) {
         super.onPlaybackStateChanged(state)
-
-        when(state){
-            Player.STATE_READY ->{
+        when (state) {
+            Player.STATE_READY -> {
                 startDateTimestamp = Date().time
             }
         }
     }
 
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        binding.buffView.onActivityResult(requestCode,resultCode,data)
+    override fun progress(position: Long) {
+        buffView.setVideoProgress(position)
+        startDateTimestamp?.let { startDateTimestamp ->
+            val currentPlaybackTimestamp =
+                startDateTimestamp + position
+            buffView.provideSync(currentPlaybackTimestamp)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        buffView.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
     }
-    companion object{
-        private const val streamID ="buffRedAppDemo"
-            //"d5ddd610-84b9-43b2-9e4f-b8edcb391e2b"
 
-        private val url = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+    companion object {
+
+        private const val PROVIDER_ID = "buffRedAppDemo"
     }
-
 }
